35a6e016efaad0bc83852b5bf879410d3a415193
anyagardner <anya@blisslandpizza.com>
2025-01-28 13:17:24 +0000

    Removed frontend and app folders

--- app/utils.py
+++
@@ -1,3 +0,0 @@
-def allowed_file(filename, allowed_extensions):
-    """Check if the file has an allowed extension."""
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
