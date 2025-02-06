969788f7fa0b2fba15ee3bde0c04777cbca6ae60
Tim.Heller <Tim.Heller@cas.de>
2025-02-06 18:39:19 +0100

    generate client for mobile

--- frontendmobileapp/app/(tabs)/index.jsx
+++ frontendmobileapp/app/(tabs)/index.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { ApiApi } from '@/generated-api';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to the Home Page!</Text>
+      <Button title='Login Test' onPress={testLogin}></Button>
     </View>
   );
+}
+
+export function testLogin() {
+  console.log("AAA")
+  axios.post(
+    'http://localhost:8080/realms/master/protocol/openid-connect/token',
+    new URLSearchParams({
+        response_type: 'token',
+        scope: 'openid',
+        client_id: 'yeslove',
+        client_secret: 'kKDqlBTi0o5NtREvVyKjf1IjisGw5jqu',
+        username: 'test-user',
+        password: 'Software1',
+        grant_type: 'password',
+    }),
+    {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    }
+).then((response) => {
+  console.log('Access Token:', response.data.access_token);
+});
+  
 }
 
 const styles = StyleSheet.create({
