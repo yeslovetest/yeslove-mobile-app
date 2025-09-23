93bfff77be596bb7ff6c61adbd30318bfe2a832e
AbubacarrCamara <AbubacarrCamara>
2025-09-23 10:10:56 +0100

    Created graph database env, config and social routes (Create user, follow, like and recommendations

--- backend/app/api/Blog/blog_models.py
+++ backend/app/api/Blog/blog_models.py
@@ -4,6 +4,5 @@ from flask_restx import fields
 CreateBlogPost = api.model("CreateBlogPost", {
     "title": fields.String(required=True, description="Title of the blog post"),
     "content": fields.String(required=True, description="Blog post content"),
-    "summary": fields.String(required=False, description="Blog post Summary /short intro to Blog"),
     "image_url": fields.String(required=False, description="Optional image URL")
 })
