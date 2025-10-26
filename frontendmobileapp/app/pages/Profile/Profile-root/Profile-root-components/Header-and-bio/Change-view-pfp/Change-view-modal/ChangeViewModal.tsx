f8cf3d3c68fc0d67397d0675cc74ace69bce166b
anyagardner <anya@blisslandpizza.com>
2025-10-26 12:47:57 +0000

    Change/view modal UI

---
+++ frontendmobileapp/app/pages/Profile/Profile-root/Profile-root-components/Header-and-bio/Change-view-pfp/Change-view-modal/ChangeViewModal.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Modal, TouchableOpacity, Animated, Text, View } from 'react-native';
+import styles from './ChangeViewModalStyles';
+
+interface ChangeViewModalProps {
+    visible: boolean;
+    onClose: () => void;
+}
+
+const ChangeViewModal: React.FC<ChangeViewModalProps> = ({ visible, onClose }) => {
+    const slideAnim = useRef(new Animated.Value(300)).current;
+    const [isRendered, setIsRendered] = useState(visible);
+
+    useEffect(() => {
+        if (visible) {
+            setIsRendered(true);
+            Animated.timing(slideAnim, {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true,
+            }).start();
+        } else {
+            Animated.timing(slideAnim, {
+                toValue: 300,
+                duration: 300,
+                useNativeDriver: true,
+            }).start(() => {
+                setIsRendered(false);
+            });
+        }
+    }, [visible]);
+    if (!isRendered) return null;
+
+
+
+    return (
+        <Modal transparent visible={isRendered} animationType="none">
+            <TouchableOpacity style={styles.backdrop} activeOpacity={1} onPress={onClose}>
+                <TouchableOpacity activeOpacity={1}>
+                    <Animated.View
+                        style={[
+                            styles.modalContent,
+                            { transform: [{ translateY: slideAnim }] },
+                        ]}
+                    >
+                        <View style={styles.settingsSubSection}>
+                            <TouchableOpacity style={styles.changeViewButton}>
+                                <Text style={styles.changeViewButtonText}>Change Profile Picture</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.changeViewButton}>
+                                <Text style={styles.changeViewButtonText}>View Profile Picture</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Animated.View>
+                </TouchableOpacity>
+            </TouchableOpacity>
+        </Modal>
+    );
+};
+
+
+export default ChangeViewModal;
--- frontendmobileapp/app/pages/Profile/Profile-root/Profile-root-components/Header-and-bio/Profile-picture/ProfilePicture.tsx
+++ frontendmobileapp/app/pages/Profile/Profile-root/Profile-root-components/Header-and-bio/Profile-picture/ProfilePicture.tsx
@@ -1,16 +1,25 @@
 import axios from 'axios'
-import React from 'react'
-import { Image } from 'react-native'
+import React, { useState } from 'react'
+import { Image, TouchableOpacity } from 'react-native'
 import styles from './ProfilePictureStyles'
 import { useAppSelector } from '@/app/store/hooks'
+import ChangeViewModal from '../Change-view-pfp/Change-view-modal/ChangeViewModal'
 
 const ProfilePicture = () => {
 const userId = useAppSelector((state) => state.navigation.tabStack.at(-1)?.data?.userId);
  const profileImage = useAppSelector((state) => state.profile.profiles[userId]?.profile_pic ?? "");
+const [changeViewModal, setChangeViewModal] = useState(false)
 
   return (
-
+    <>
+     <TouchableOpacity onPress={() => setChangeViewModal(true)} >
     <Image style={styles.profileImage} source={{ uri:   axios.defaults.baseURL + "/api/media/" +  profileImage}} />
+        </TouchableOpacity>
+        
+                <ChangeViewModal  visible={changeViewModal}
+                    onClose={() => setChangeViewModal(false)}></ChangeViewModal>
+
+        </>
   )
 }
 
