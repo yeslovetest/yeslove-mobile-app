774b68430c1d3f2badce49fd6273b9d01fef552a
Leo Badigra <badigraachailletsaturnin@yahoo.fr>
2026-01-03 22:15:56 +0000

    Fix profile API validation and prevent 400/500 errors

--- backend/app/api/profile/profile_models.py
+++ backend/app/api/profile/profile_models.py
@@ -37,7 +37,7 @@ AboutResponse = api.model("AboutResponse", {
     }, strict=False)
 
 UserQuery = api.model("UserQuery", {
-    "username": fields.String(required=True, descriptions="User's username (Required)"),
+    "username": fields.String(required=True, description="User's username (Required)"),
     "email": fields.String(description="User's email (Optional)"),
     "user_id": fields.Integer(description="User's database ID (Optional)"),
     })
@@ -90,5 +90,6 @@ TimelineResponse = api.model('TimelineResponse',
     "current_page": fields.Integer(description="Current page number"),
     "posts": fields.List(fields.Nested(UserPost), description="List of posts"),
 })
+
 
 
