ea5b7bb549b92e6a9b9b233552730e5bf85069a7
ugo <ugooaghanya98@gmail.com>
2026-03-28 20:25:51 +0000

    Refine events cards and move filter to header
    (cherry picked from commit f1d56d6e3552b7206378e5ab228417c2b9ea9739)

--- frontendmobileapp/app/pages/Events/Events-root/Events-root-components/Attended/AttendedContent.tsx
+++ frontendmobileapp/app/pages/Events/Events-root/Events-root-components/Attended/AttendedContent.tsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { View } from 'react-native'
 import { useFocusEffect } from 'expo-router'
 import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
 import { fetchUserEvents } from '@/app/store/Events-store/eventsSlice';
 import EventsList from '../Events-list/EventsList';
-import DateFilterDropdown from '../DateFilter/DateFilter';
 import EventPaginator from '../../../../../Universal-components/Paginator/Paginator';
 
-const AttendedContent = () => {
+interface Props {
+    dates: { startDate?: string; endDate?: string };
+}
+
+const AttendedContent = ({ dates }: Props) => {
 
     const dispatch = useAppDispatch();
-    const [dates, setDates] = useState<{ startDate?: string; endDate?: string }>({});
     const currentPage = useAppSelector(state => state.events.userEvents.eventPage);
     const totalEvents = useAppSelector(state => state.events.userEvents.totalEvents);
     const eventsPerPage = useAppSelector(state => state.events.userEvents.eventsPerPage);
     const totalPages = Math.ceil(totalEvents / eventsPerPage); // Calculate total pages
     
     useFocusEffect(React.useCallback(() => {
-        dispatch(fetchUserEvents({queryType: 'attended'}));
-    }, []));
-
-    const handleSearch = (dates: { startDate?: string; endDate?: string }) => { 
         dispatch(fetchUserEvents({...dates, queryType: 'attended'}));
-        setDates(dates); // store date to be used by paginator
-    };
+    }, [dates.endDate, dates.startDate]));
 
     const handlePageChange = (page: number) => {
         dispatch(fetchUserEvents({...dates, queryType: 'attended', currentPage: page }));
@@ -31,7 +28,6 @@ const AttendedContent = () => {
 
     return (
         <View style={{ width: '100%' }}>
-            <DateFilterDropdown onSearch={handleSearch} />
             <EventsList eventType='attended'/>
                 {totalPages > 0 && (
                     <EventPaginator currentPage={currentPage ?? 1} totalPages={totalPages ?? 1} 
--- frontendmobileapp/app/pages/Events/Events-root/Events-root-components/One-event/OneEventStyles.tsx
+++ frontendmobileapp/app/pages/Events/Events-root/Events-root-components/One-event/OneEventStyles.tsx
@@ -2,61 +2,60 @@ import theme from "@/assets/variables/Variables";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-        eventsContainer: {
-        width: theme.spacing.postWidth,
+    eventsContainer: {
+        width: "95%",
         display: "flex",
         justifyContent: "flex-start",
         alignItems: "center",
     },
-eventContainer: {
-        width: "90%",
-    minHeight: 150,
-        position: "relative",
+    eventContainer: {
+        width: "100%",
+        height: 185,
         display: "flex",
-        alignItems: "flex-end",
+        alignItems: "flex-start",
         justifyContent: "flex-end",
         backgroundColor: theme.colors.eventTextBackground,
         marginBottom: 18,
         borderRadius: 15,
-
+        overflow: "hidden",
     },
 
     eventName: {
-        paddingHorizontal: 10,
         fontSize: 18,
         color: theme.colors.bannerTextColor,
-        fontWeight: '600'
+        fontWeight: '700',
+        lineHeight: 22,
     },
     eventLocation: {
-        paddingHorizontal: 10,
-        fontSize: 16,
+        marginTop: 4,
+        fontSize: 15,
         color: theme.colors.bannerTextColor,
-        marginBottom: 10,
+        marginBottom: 2,
         fontWeight: '400'
     },
 
     dateContainer: {
         position: "absolute",
-        height: 55,
-        width: 80,
-        top: 48,
-        left: -20,
+        height: 48,
+        width: 68,
+        top: 12,
+        left: 12,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: theme.colors.primaryBlue,
-        borderRadius: 10,
+        backgroundColor: 'rgba(45, 91, 227, 0.88)',
+        borderRadius: 12,
         zIndex: 2,
     },
     dateText: {
         color: theme.colors.bannerTextColor,
-        fontSize: 14,
+        fontSize: 13,
         fontWeight: '600',
         textAlign: "center"
     },
     yearText: {
         color: theme.colors.bannerTextColor,
-        fontSize: 11,
+        fontSize: 10,
         textAlign: "center",
         fontWeight: '400'
     },
@@ -64,13 +63,22 @@ eventContainer: {
         width: "100%",
         height: "100%",
     },
+    eventImageStyle: {
+        borderRadius: 10,
+    },
     overlay: {
         ...StyleSheet.absoluteFillObject,
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        backgroundColor: 'rgba(0, 0, 0, 0.38)',
         borderRadius: 10,
         display: "flex",
-        alignItems: "flex-end",
+        alignItems: "flex-start",
         justifyContent: "flex-end",
+        paddingHorizontal: 14,
+        paddingBottom: 12,
+    },
+    eventTextBlock: {
+        width: "100%",
+        paddingRight: 8,
     },
 
 
